34b2b23a4c47ac66231e7c12bad5ec66f0486972
souravkairy <48748661+souravkairy@users.noreply.github.com>
2023-02-15 02:20:29 +0600

    Update function blockquote_social_sharing_butto.php

--- function blockquote_social_sharing_butto.php
+++ function blockquote_social_sharing_butto.php
@@ -10,110 +10,121 @@ Happy coading -->
 
 <?php
 
-function add_font_awesome_cdn() {
-  wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.14.0/css/all.css' );
-}
-add_action( 'wp_enqueue_scripts', 'add_font_awesome_cdn' );
+<?php
+/**
+ * GeneratePress child theme functions and definitions.
+ *
+ * Add your custom PHP in this file.
+ * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
+ */
 
-function blockquote_social_sharing_buttons_copy() {
-  ?>
-  <style>
-    .quote {
-      position: relative;
-      padding: 1em;
-      background-color: #f9f9f9;
-      border-left: 0.25em solid #ccc;
-      margin-bottom: 1em;
-    }
+  function add_font_awesome_cdn() {
+    wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.14.0/css/all.css' );
+  }
+  add_action( 'wp_enqueue_scripts', 'add_font_awesome_cdn' );
+  
+  function blockquote_social_sharing_buttons_copy() {
+    ?>
+    <style>
+      .quote {
+        position: relative;
+        padding: 1em;
+        background-color: #f9f9f9;
+        border-left: 0.25em solid #ccc;
+        margin-bottom: 1em;
+      }
+  
+      .quote .actions {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        margin-top: 1em;
+      }
+  
+      .quote .actions .btn {
+        display: inline-block;
+        margin-right: 0.5em;
+        padding: 1px 18px;
+        color: #fff;
+        border: 0;
+        border-radius: 16px;
+        cursor: pointer;
+      }
+      .mr-5 {
+        margin-right: 0.5em;
+      }
+      .quote .actions .fb {
+        background-color: #4267B2;
+      }
 
-    .quote .actions {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      margin-top: 1em;
-    }
+      .quote .actions .fb:hover {
+        opacity: 0.8;
+      }
 
-    .quote .actions .btn {
-      display: inline-block;
-      margin-right: 0.5em;
-      padding: 1px 18px;
-      color: #fff;
-      border: 0;
-      border-radius: 16px;
-      cursor: pointer;
-    }
-    .mr-5 {
-      margin-right: 0.5em;
-    }
-    .quote .actions .fb {
-      background-color: #4267B2;
-    }
+      .quote .actions .wh {
+        background-color: #25D366;
+      }
 
-    .quote .actions .fb:hover {
-      opacity: 0.8;
-    }
+      .quote .actions .wh:hover {
+        opacity: 0.8;
+      }
+      
+      .quote .actions .btn.copy {
+        background: linear-gradient(90deg , rgba(253,29,29,1) 27%, rgba(252,176,69,1) 100%);
+      }
 
-    .quote .actions .wh {
-      background-color: #25D366;
-    }
+      .quote .actions .btn.copy:hover {
+        opacity: 0.8;
+      }
 
-    .quote .actions .wh:hover {
-      opacity: 0.8;
-    }
-    
-    .quote .actions .btn.copy {
-      background: linear-gradient(90deg , rgba(253,29,29,1) 27%, rgba(252,176,69,1) 100%);
-    }
-
-    .quote .actions .btn.copy:hover {
-      opacity: 0.8;
-    }
-
-  </style>
-
-  <script>
-  document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.quote .btn.copy').forEach(function(button) {
-      button.addEventListener('click', function() {
-        var quote = button.closest('.quote').querySelector('p').innerText;
-        navigator.clipboard.writeText(quote).then(function() {
-          alert('Quote copied to clipboard.');
+    </style>
+  
+    <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      document.querySelectorAll('.quote .btn.copy').forEach(function(button) {
+        button.addEventListener('click', function() {
+            
+          var quote = button.closest('.quote').querySelector('span').innerText;
+          navigator.clipboard.writeText(quote).then(function() {
+            alert('Quote copied to clipboard.');
+          });
         });
       });
     });
-  });
-  </script>
-
-  <?php
-}
-add_action( 'wp_footer', 'blockquote_social_sharing_buttons_copy' );
-
-function add_social_sharing_buttons_copy_to_blockquote( $content ) {
-  $content = preg_replace_callback( '/<blockquote.*?>(.*?)<\/blockquote>/is', function( $matches ) {
-    $quote = strip_tags( $matches[1] );
-     $quote1 = $matches[1];
-    $url = urlencode( get_permalink() );
-    $share_links = sprintf(
-      '<div class="actions">
-      <span class="mr-5">Share-</span>
-        <a class="btn fb" href="https://www.facebook.com/sharer/sharer.php?u=%1$s" target="_blank"><i class="fab fa-facebook-f"></i></a>
-        <a class="btn wh" href="https://api.whatsapp.com/send?text=%2$s%%20%1$s" target="_blank"><i class="fab fa-whatsapp"></i></a>
-        <button class="btn copy">COPY</button>
-            </div>',
-      $url,
-      urlencode( $quote )
-    );
-    return sprintf(
-      '<div class="quote">
-        <p>%s</p>
-        %s
-      </div>',
-      $quote1,
-      $share_links
-    );
-  }, $content );
-
-  return $content;
-}
-add_filter( 'the_content', 'add_social_sharing_buttons_copy_to_blockquote' );
+    </script>
+  
+    <?php
+  }
+  add_action( 'wp_footer', 'blockquote_social_sharing_buttons_copy' );
+  
+  function add_social_sharing_buttons_copy_to_blockquote( $content ) {
+    $content = preg_replace_callback( '/<blockquote.*?>(.*?)<\/blockquote>/is', function( $matches ) {
+      $quote = strip_tags( $matches[1] );
+       $quote1 = $matches[1];
+      $url = urlencode( get_permalink() );
+      $share_links = sprintf(
+        '<div class="actions">
+        <span class="mr-5">Share-</span>
+          <a class="btn fb" href="https://www.facebook.com/sharer/sharer.php?u=%1$s" target="_blank"><i class="fab fa-facebook-f"></i></a>
+          <a class="btn wh" href="https://api.whatsapp.com/send?text=%2$s%%20%1$s" target="_blank"><i class="fab fa-whatsapp"></i></a>
+          <button class="btn copy">COPY</button>
+              </div>',
+        $url,
+        urlencode( $quote )
+      );
+      return sprintf(
+        '<div class="quote">
+          <p>%s</p>
+          <span style ="display:none">%s</span>
+          %s
+        </div>',
+        $quote1,
+        $quote,
+        $share_links
+      );
+    }, $content );
+  
+    return $content;
+  }
+  add_filter( 'the_content', 'add_social_sharing_buttons_copy_to_blockquote' );
